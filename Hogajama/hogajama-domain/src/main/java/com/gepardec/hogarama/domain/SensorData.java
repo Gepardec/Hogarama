d7bab386872bf834a87fd2991dc7bd7f4f73f6c3
Sabrina <sabrina@sjutz.gepardec.com>
2017-02-17 15:26:39 +0100

    DAO

---
+++ Hogajama/hogajama-domain/src/main/java/com/gepardec/hogarama/domain/SensorData.java
@@ -0,0 +1,22 @@
+package com.gepardec.hogarama.domain;
+
+public class SensorData {
+
+	private double temp;
+	private String comment;
+	
+	
+	public double getTemp() {
+		return temp;
+	}
+	public void setTemp(double temp) {
+		this.temp = temp;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+}
