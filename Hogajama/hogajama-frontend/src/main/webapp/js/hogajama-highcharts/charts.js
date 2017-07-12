18a9620dc565a199ff692e544f1f90e656d3688e
Matthias Kapferer <matthias.kapferer@gepardec.com>
2017-07-12 15:58:25 +0200

    #52 Chart updates data every 2.5 seconds

--- Hogajama/hogajama-frontend/src/main/webapp/js/hogajama-highcharts/charts.js
+++ Hogajama/hogajama-frontend/src/main/webapp/js/hogajama-highcharts/charts.js
@@ -4,63 +4,68 @@
 
 $(document).ready(function (e) { 
 	
-	
-	var habarama_system_uptime = 0;
-//	$.get("/hogajama-rs/rest/helloworld/mongodb/1", function(response){
-//    	//console.log(response.system_uptime);
-//		if(response.system_uptime > 0) {
-//			habarama_system_uptime = response.system_uptime;
-//		}
-//    });
-	var sensors = [];
-	var sensorDaten = [];
-    $.ajax({
-        url: '/hogajama-rs/rest/sensor',
-        success: function (response) {
-            sensors = response;
-        },
-        async: false
-    });
 
-    for(var i = 0; i < sensors.length; i++){
+    function loadChartData(max){
+
+        if (typeof max == 'undefined'){
+            max = 30;
+        }
+
+        var sensors = [];
+        var sensorData = [];
         $.ajax({
-            url: 'hogajama-rs/rest/sensor/allData?maxNumber=30&sensor=' + sensors[i],
+            url: '/hogajama-rs/rest/sensor',
             success: function (response) {
-                sensorDaten.push(response);
+                sensors = response;
             },
             async: false
         });
+
+        for(var i = 0; i < sensors.length; i++){
+            $.ajax({
+                url: 'hogajama-rs/rest/sensor/allData?maxNumber=' + max + '&sensor=' + sensors[i],
+                success: function (response) {
+                    sensorData.push(response);
+                },
+                async: false
+            });
+        }
+        var times = [];
+        var series = [];
+        for(var i = 0; i < sensorData[0].length; i++){
+            times.push(sensorData[0][i]['time'].match(/T(\d\d:\d\d)/)[1]);
+        }
+
+        for(var i = 0; i < sensorData.length; i++){
+
+            var values = [];
+
+            for(var j = 0; j < sensorData[i].length; j++){
+                values.push(sensorData[i][j]['value']);
+            }
+
+            var serie = {
+                name: sensors[i] + " " + sensorData[i][0].location,
+                data: values.reverse()
+            };
+            series.push(serie);
+        }
+
+        return [sensors, sensorData, times, series];
+
     }
+
+	// load data for the first time here
+    console.log("Load chart data first time");
+    var [sensors, sensorData, times, series] = loadChartData();
 
     $("#spinner").addClass('hidden');
     $("#moisture-chart").removeClass('hidden');
 
-
-    var times = [];
-    var series = [];
-
-    var datum = sensorDaten[0][0]['time'].match(/\d\d\d\d-\d\d-\d\d/)[0].split("-");
+    var datum = sensorData[0][0]['time'].match(/\d\d\d\d-\d\d-\d\d/)[0].split("-");
     datum = datum[2] + "." + datum[1] + "." + datum[0];
 
-    for(var i = 0; i < sensorDaten[0].length; i++){
-        times.push(sensorDaten[0][i]['time'].match(/T(\d\d:\d\d)/)[1]);
-    }
-
-    for(var i = 0; i < sensorDaten.length; i++){
-
-        var values = [];
-
-        for(var j = 0; j < sensorDaten[i].length; j++){
-            values.push(sensorDaten[i][j]['value']);
-        }
-
-        var serie = {
-            name: sensors[i] + " " + sensorDaten[i][0].location,
-            data: values.reverse()
-        };
-        series.push(serie);
-    }
-
+    console.log("Create chart");
     var moistureChart = Highcharts.chart('moisture-chart', {
             chart: {
                 type: 'line'
@@ -89,5 +94,21 @@ $(document).ready(function (e) {
             },
             series: series
         });
+
+    setInterval(function(){
+        // Udate chart with the current data
+        console.log("Update chart data");
+        var [newSensors, newSensorData, newTimes, newSeries] = loadChartData();
+
+        // Update time
+        moistureChart.xAxis[0].setCategories(newTimes.reverse(), true);
+        // Update data
+        moistureChart.update({
+            series: newSeries
+        }, true);
+
+        //moistureChart.redraw();
+
+    }, 2500);
     
 });
