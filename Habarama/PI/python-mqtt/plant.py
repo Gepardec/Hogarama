8638b5d23a0fa63261689fd181100542103d7ceb
AdrianFarmadin <adrian.farmadin@gepardec.com>
2017-08-04 12:23:51 +0200

    Python client: ignore connection errors and carry on

--- Habarama/PI/python-mqtt/plant.py
+++ Habarama/PI/python-mqtt/plant.py
@@ -32,18 +32,21 @@ ssl_ctx = ssl.create_default_context(cafile='./broker.pem')
 ssl_ctx.check_hostname = False
 client.tls_set_context(ssl_ctx)
 client.username_pw_set("mq_habarama", "mq_habarama_pass")
-client.connect("broker-amq-mqtt-ssl-57-hogarama.cloud.itandtel.at", 443, 60)
-# client.disconnect()
 
 # Main program loop.
 while True:
-   GPIO.output(inPin, 1)
-   time.sleep(sampleInterval)
-   watterLevel = mcp.read_adc(sensorChannel)
-   percent = 100 - int(round(watterLevel/10.24))
-   print "ADC Output: {0:4d} Percentage: {1:3}%".format (watterLevel,percent)
-   payload = '{{"sensorName": "{}", "type": "water", "value": {}, "location": "Wien", "version": 1 }}'
-   payload = payload.format(subjectName,percent)
-   client.publish("habarama", payload=payload, qos=0, retain=False)
-   GPIO.output(inPin, 0)
-   time.sleep(waitInterval)
+    try:
+        client.connect("broker-amq-mqtt-ssl-57-hogarama.cloud.itandtel.at", 443, 60)
+        GPIO.output(inPin, 1)
+        time.sleep(sampleInterval)
+        watterLevel = mcp.read_adc(sensorChannel)
+        percent = 100 - int(round(watterLevel/10.24))
+        print "ADC Output: {0:4d} Percentage: {1:3}%".format (watterLevel,percent)
+        payload = '{{"sensorName": "{}", "type": "water", "value": {}, "location": "Wien", "version": 1 }}'
+        payload = payload.format(subjectName,percent)
+        client.publish("habarama", payload=payload, qos=0, retain=False)
+        GPIO.output(inPin, 0)
+        client.disconnect()
+    except:
+        print "Oops! Something wrong. Trying luck in next iteration."
+    time.sleep(waitInterval)
