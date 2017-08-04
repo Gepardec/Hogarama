7b532d9240eb2e7a059539e419ea218c22a5b0f5
Sabrina Jutz <sabrina.jutz@gepardec.com>
2017-08-04 09:37:05 +0200

    #57 test using env variables in hogajama

--- Hogajama/hogajama-services/src/main/java/com/gepardec/hogarama/service/MongoDbProducer.java
+++ Hogajama/hogajama-services/src/main/java/com/gepardec/hogarama/service/MongoDbProducer.java
@@ -20,7 +20,7 @@ public class MongoDbProducer {
 
 	private static final String HOGAJAMA_DB = "hogajamadb";
 	private static final String USER = "hogajama";
-	private static final char[] PASSWORD = "hogajama@mongodb".toCharArray();
+	private static final char[] PASSWORD = System.getProperty("MONGODB_PW").toCharArray();
 	private static final int PORT = 27017;
 	private static final String COLLECTION = "habarama";
 	private static final String HOST = System.getProperty("mongo.host", "mongodb");
