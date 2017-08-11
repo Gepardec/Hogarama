69153287d26e76b90b2ab1624364c0e4a8491b40
Erhard Siegl <erhard.siegl@gepardec.com>
2017-08-11 21:03:55 +0200

    #50 remove unnecessary template

--- template/local_openshift/startAll.sh
+++ template/local_openshift/startAll.sh
@@ -6,7 +6,6 @@ oc cluster up
 oc login -u system:admin
 oc policy add-role-to-user system:image-pusher developer
 oc create -f alltemplates.yaml -n openshift
-oc create -f hogaramaimagestreams.yaml -n openshift
 oc login -u developer
 oc new-project hogarama
 oc create is hogajama
