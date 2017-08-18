fb25506f310da4d7298995fb785d20c9a03e2152
Herbert Wirnsberger <herbert.wirnsberger@gepardec.com>
2017-08-18 12:44:59 +0200

    #50 removed imagestream debian. it will be downloaded automatically.

--- template/local_openshift/startAll.sh
+++ template/local_openshift/startAll.sh
@@ -10,10 +10,4 @@ oc login -u developer
 oc new-project hogarama
 oc create is hogajama
 oc create is fluentd
-oc create is debian
 oc process -f hogaramaOhneHost.yaml | oc create -f -
-docker pull debian:stretch-slim
-OPENSHIFT_TOKEN=$(oc whoami -t)
-docker login -u developer -p ${OPENSHIFT_TOKEN} 172.30.1.1:5000
-docker tag debian:stretch-slim 172.30.1.1:5000/hogarama/debian:stretch-slim
-docker push 172.30.1.1:5000/hogarama/debian:stretch-slim
