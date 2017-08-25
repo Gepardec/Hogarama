14361eb807574060aac342466dfd903db657f298
Sabrina Jutz <sabrina.jutz@gepardec.com>
2017-08-25 08:11:25 +0200

    #55 add env to templates

--- template/make-hogarama.sh
+++ template/make-hogarama.sh
@@ -14,7 +14,7 @@ for instance in "${arguments[@]}"
 do
   #TODO: create new project
   #TODO: check if project creation has been successfull
-  # oc process -f hogarama_template_with_routes.yaml STAGE=$instance | oc create -f -   #TODO: activate
-  echo $instance
+  OPENSHIFT_TOKEN=$(oc whoami -t)
+  #oc process -f hogarama_template_with_routes.yaml STAGE=$instance OPENSHIFT_AUTH_TOKEN=$OPENSHIFT_TOKEN | oc create -f -   #TODO: activate
 done
 
