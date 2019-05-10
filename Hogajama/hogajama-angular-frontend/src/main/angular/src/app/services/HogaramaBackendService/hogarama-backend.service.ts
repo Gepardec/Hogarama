aa749194e44e74f127da7d94643dbc800ad99883
Maurice el-Banna <maurice.elbanna@gepardec.com>
2019-05-10 16:40:07 +0200

    #206 Removes localhost in dev-mode due to maven building in dev mode

--- Hogajama/hogajama-angular-frontend/src/main/angular/src/app/services/HogaramaBackendService/hogarama-backend.service.ts
+++ Hogajama/hogajama-angular-frontend/src/main/angular/src/app/services/HogaramaBackendService/hogarama-backend.service.ts
@@ -9,9 +9,9 @@ export class HogaramaBackendService {
   public baseUrl: string = 'https://hogajama-57-hogarama.cloud.itandtel.at/hogajama-rs';
 
   constructor(private http: HttpClient) {
-    if (isDevMode()) {
+    /*if (isDevMode()) {
       this.baseUrl = 'http://localhost:8080/hogajama-rs';
-    }
+    }*/
   }
 
   public getAllSensors(): Observable<string[]> {
