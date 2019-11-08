da4f4c217222c02d71f3b4f20582c835012b8c25
Egor Erofeev <egor.erofeev@gepardec.com>
2019-11-08 11:24:10 +0100

    Intermediate

--- Hogajama/hogajama-domain/src/main/java/com/gepardec/hogarama/domain/sensor/SensorDAO.java
+++ Hogajama/hogajama-domain/src/main/java/com/gepardec/hogarama/domain/sensor/SensorDAO.java
@@ -1,14 +1,11 @@
 package com.gepardec.hogarama.domain.sensor;
 
-import java.util.Date;
+import com.gepardec.hogarama.domain.GenericDao;
+import com.gepardec.hogarama.domain.entity.Sensor;
+
 import java.util.List;
 
-public interface SensorDAO {
+public interface SensorDao extends GenericDao<Sensor> {
 
-    /** Constant to use for querying all sensors */
-	String ALL_SENSORS = "";
-	
-    public List<String> getAllSensors();
-	public List<SensorData> getAllData(Integer maxNumber, String sensorName, Date from, Date to);
-	public String getLocationBySensorName(String sensorName);
+    List<Sensor> getAllSensorForOwner(Long ownerId);
 }
