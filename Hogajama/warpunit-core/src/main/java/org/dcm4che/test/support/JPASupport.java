3bd2b3b17236717c67718240a65c2bda5d93ea69
Patrick Holzer <patrick.holzer@gepardec.com>
2020-04-03 12:56:36 +0200

    #286 Remove WarpUnit and IT

--- Hogajama/warpunit-core/src/main/java/org/dcm4che/test/support/JPASupport.java
+++
@@ -1,9 +0,0 @@
-package org.dcm4che.test.support;
-
-import java.util.function.Supplier;
-
-public interface JPASupport {
-	public void beginTx();
-	public void commitTx();
-	public <T> T unwrap(T jpaEntity, Class<T> clazz);
-}
