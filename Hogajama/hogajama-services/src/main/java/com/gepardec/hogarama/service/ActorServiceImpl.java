466682dcdcb49d4989fe67bfcc51eee53ada35ee
Erhard Siegl <erhard.siegl@gepardec.com>
2020-04-24 15:35:12 +0200

    #323 Dont read MongoDB for checking sensor location when writing to actor. This is slow and makes no sense.

--- Hogajama/hogajama-services/src/main/java/com/gepardec/hogarama/service/ActorServiceImpl.java
+++ Hogajama/hogajama-services/src/main/java/com/gepardec/hogarama/service/ActorServiceImpl.java
@@ -1,23 +1,20 @@
 package com.gepardec.hogarama.service;
 
-import com.gepardec.hogarama.domain.metrics.Metrics;
-import com.gepardec.hogarama.domain.sensor.SensorDAO;
-import com.gepardec.hogarama.domain.watering.ActorService;
-import com.gepardec.hogarama.domain.watering.WateringData;
-import com.gepardec.hogarama.mocks.cli.MqttClient;
+import java.util.Date;
+import java.util.Optional;
+
+import javax.inject.Inject;
+
 import org.json.JSONObject;
 import org.mongodb.morphia.Datastore;
 import org.slf4j.Logger;
 
-import javax.inject.Inject;
-import java.util.Date;
-import java.util.Optional;
+import com.gepardec.hogarama.domain.metrics.Metrics;
+import com.gepardec.hogarama.domain.watering.ActorService;
+import com.gepardec.hogarama.domain.watering.WateringData;
+import com.gepardec.hogarama.mocks.cli.MqttClient;
 
 public class ActorServiceImpl implements ActorService {
-
-
-    @Inject
-    private SensorDAO sensorDAO;
 
     @Inject
     private Datastore db;
@@ -27,38 +24,27 @@ public class ActorServiceImpl implements ActorService {
 
 
     @Override
-    public void sendActorMessage(String location, String sensorName, Integer duration) {
+    public void sendActorMessage(String location, String actorName, Integer duration) {
 
-        log.info("sendActorMessage: location: {}, actorName: {}, duration: {}", location, sensorName, duration);
-        checkParametersOrFail(location, sensorName, duration);
+        log.info("sendActorMessage: location: {}, actorName: {}, duration: {}", location, actorName, duration);
+        checkParametersOrFail(location, actorName, duration);
 
         MqttClient mqttClient = new MqttClient().defaultConnection().
-                withTopic(Optional.ofNullable(System.getenv("AMQ_TOPICS")).orElse("actor." + location + "." + sensorName)).
+                withTopic(Optional.ofNullable(System.getenv("AMQ_TOPICS")).orElse("actor." + location + "." + actorName)).
                 build();
 
 
-        WateringData data = new WateringData(new Date(), sensorName, location, duration);
+        WateringData data = new WateringData(new Date(), actorName, location, duration);
         db.save(data);
         JSONObject json = new JSONObject(data);
         String message = json.toString();
         mqttClient.connectAndPublish(message);
     }
 
-    protected void checkParametersOrFail(String location, String sensorName, Integer duration) {
-        if (location == null || location.isEmpty() || sensorName == null || sensorName.isEmpty() || duration == null) {
+    protected void checkParametersOrFail(String location, String actorName, Integer duration) {
+        if (location == null || location.isEmpty() || actorName == null || actorName.isEmpty() || duration == null) {
             Metrics.exceptionsThrown.labels("hogarama_services", "IllegalArgumentException", "ActorServiceImpl.checkParametersOrFail").inc();
-            throw new IllegalArgumentException(String.format("Supplied parameters '%s', '%s', '%s' must not be empty or null", location, sensorName, duration));
+            throw new IllegalArgumentException(String.format("Supplied parameters '%s', '%s', '%s' must not be empty or null", location, actorName, duration));
         }
-
-        String registeredLocation = sensorDAO.getLocationBySensorName(sensorName);
-        if (registeredLocation == null) {
-            Metrics.exceptionsThrown.labels("hogarama_services", "IllegalArgumentException", "ActorServiceImpl.checkParametersOrFail").inc();
-            throw new IllegalArgumentException(sensorName + " is not a registered sensor.");
-        }
-        if (!registeredLocation.equals(location)) {
-            Metrics.exceptionsThrown.labels("hogarama_services", "IllegalArgumentException", "ActorServiceImpl.checkParametersOrFail").inc();
-            throw new IllegalArgumentException(String.format("For sensor %s location must be '%s' but was '%s'", sensorName, registeredLocation, location));
-        }
-
     }
 }
