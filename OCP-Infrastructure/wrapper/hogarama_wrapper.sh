903a9e486fd985c14cfca18f2a212b128c943e2d
fhochleitner <Ka2Us6m1clvV>
2020-06-18 10:02:30 +0200

    make hogarama setup script compatable with different container engines

--- OCP-Infrastructure/wrapper/hogarama_wrapper.sh
+++ OCP-Infrastructure/wrapper/hogarama_wrapper.sh
@@ -6,10 +6,16 @@
 
 readonly SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 readonly TOPLEVEL_DIR=$( cd ${SCRIPT_DIR}/.. > /dev/null && pwd )
+ENGINE=${CONTAINER_ENGINE}
 
 main () {
+
+  if [[ "x${ENGINE}" == "x" ]]; then
+    ENGINE=docker
+  fi
+
   set -e
-  docker run --rm -it \
+  ${ENGINE} run --rm -it \
     -v ${TOPLEVEL_DIR}/:/mnt/hogarama \
     -v ~/.kube/config/:/.kube/config \
     gepardec/hogarama-bootstrap:1.0.0\
