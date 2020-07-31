dfbb79569140af7315a78d53286c73aa72d3d4df
MauriceNino <mauriceprivat98@gmail.com>
2020-07-31 07:44:09 +0200

    #319 Cleanup

--- Hogajama/hogajama-angular-frontend/src/main/ionic/src/app/modules/test-prototype/test-prototype.component.ts
+++ Hogajama/hogajama-angular-frontend/src/main/ionic/src/app/modules/test-prototype/test-prototype.component.ts
@@ -30,45 +30,11 @@ export class TestPrototypeComponent implements OnInit {
       this.units = await this.rs.units.getAllByBearer();
     } catch(error) {
       console.error(error);
-
-      // For testing
-      this.units = [
-        {
-          isDefault: true,
-          id: 1
-        },{
-          isDefault: false,
-          id: 2,
-          name: 'Gummibaum'
-        }
-      ]
     }
     try {
       this.sensors = await this.rs.sensors.getAllByBearer();
     } catch(error) {
       console.error(error);
-
-      // For testing
-      this.sensors = [
-        {
-          id: 1,
-          unitId: 1,
-          sensorTypeId: 1,
-          name: 'Sensor 1'
-        },
-        {
-          id: 2,
-          unitId: 1,
-          sensorTypeId: 1,
-          name: 'Sensor 2'
-        },
-        {
-          id: 3,
-          unitId: 2,
-          sensorTypeId: 1,
-          name: 'Sensor 3'
-        }
-      ]
     }
 
     [this.defaultUnit, ...this.complUnits] = getCompleteUnits(this.units, this.sensors, []);
@@ -77,16 +43,6 @@ export class TestPrototypeComponent implements OnInit {
       this.username = (await this.rs.users.getByBearer()).name;
     } catch(error) {
       console.error(error);
-    }
-
-    // TODO do we want to redirect to the dashboard if we already have some sensors, units and actors?
-    // Answer: no we want to stay on the same page and rename it to 'dashboard' ;)
-    // this.redirectToDashboardIfNecessary();
-  }
-
-  private redirectToDashboardIfNecessary() {
-    if (this.units.length != 0 || this.sensors.length != 0) {
-      this.router.navigateByUrl("/dashboard");
     }
   }
 
