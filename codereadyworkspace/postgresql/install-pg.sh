9595f24c18b9b6750813c498fa5471f0f7413949
ext_herbert.wirnsber <herbert.wirnsberger@gepardec.com>
2020-08-21 18:51:59 +0200

    fix variables

--- codereadyworkspace/postgresql/install-pg.sh
+++ codereadyworkspace/postgresql/install-pg.sh
@@ -5,7 +5,7 @@ source ${ENV_FILE}
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm upgrade -i postgresql bitnami/postgresql --version=9.3.2 \
     --set global.postgresql.postgresqlDatabase=management \
-    --set global.postgresql.postgresqlUsername=${POSTGRESQL_USERNAME} \
+    --set global.postgresql.postgresqlUsername=hogajama \
     --set global.postgresql.postgresqlPassword=${POSTGRESQL_PASSWORD} \
     --set persistence.enabled=false \
     --set securityContext.enabled=false
