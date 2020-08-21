28df91cfc5e32cbd90656bbd63fcd6056acf4abf
ext_herbert.wirnsber <herbert.wirnsberger@gepardec.com>
2020-08-21 17:50:58 +0200

    merge devfiles

---
+++ codereadyworkspace/postgresql/install-pg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source ${ENV_FILE}
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm upgrade -i postgresql bitnami/postgresql --version=9.3.2 \
+    --set global.postgresql.postgresqlDatabase=management \
+    --set global.postgresql.postgresqlUsername=${POSTGRESQL_USERNAME} \
+    --set global.postgresql.postgresqlPassword=${POSTGRESQL_PASSWORD} \
+    --set persistence.enabled=false \
+    --set securityContext.enabled=false
