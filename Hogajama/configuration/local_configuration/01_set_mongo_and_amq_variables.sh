db9c519016c2459c6ba2a424122301a30ab4d951
hwirnsberger <herbert.wirnsberger@gepardec.com>
2020-08-22 13:08:18 +0000

    use installed amq

--- Hogajama/configuration/local_configuration/01_set_mongo_and_amq_variables.sh
+++
@@ -1,5 +0,0 @@
-echo "export AMQ_HOST=https://broker-amq-mqtt-ssl-hogarama.127.0.0.1.nip.io" >> $JBOSS_HOME/bin/standalone.conf
-echo "export AMQ_NETTY_HOST=localhost" >> $JBOSS_HOME/bin/standalone.conf
-
-echo "set AMQ_HOST=https://broker-amq-mqtt-ssl-hogarama.127.0.0.1.nip.io" >> $JBOSS_HOME/bin/standalone.conf.bat
-echo "set AMQ_NETTY_HOST=localhost" >> $JBOSS_HOME/bin/standalone.conf.bat
