788b3f4a4556e20f395656ee0e8bd9201f9656a2
Erhard Siegl <erhard.siegl@gepardec.com>
2021-04-16 14:21:55 +0200

    Move to AWS

--- Hogajama/configuration/local_configuration/01_port_forward_mongo.sh
+++ Hogajama/configuration/local_configuration/01_port_forward_mongo.sh
@@ -1,3 +1,3 @@
-MONGODB=$(oc get pods -l name=mongodb --no-headers=true | grep Running | cut -d" " -f1)
+MONGODB=$(oc get pods -l app.kubernetes.io/component=mongodb --no-headers=true | grep Running | cut -d" " -f1)
 oc port-forward $MONGODB 27017 &
 
--- Hogajama/configuration/local_configuration/01_set_keycloak_variables.sh
+++ Hogajama/configuration/local_configuration/01_set_keycloak_variables.sh
@@ -1,2 +1,2 @@
-echo "export KEYCLOAK_AUTH_SERVER_URL=https://secure-sso-57-hogarama.cloud.itandtel.at/auth/" >> $JBOSS_HOME/bin/standalone.conf
-echo "set KEYCLOAK_AUTH_SERVER_URL=https://secure-sso-57-hogarama.cloud.itandtel.at/auth/" >> $JBOSS_HOME/bin/standalone.conf.bat
+echo "export KEYCLOAK_AUTH_SERVER_URL=https://keycloak-gepardec.apps.p.aws.ocp.gepardec.com/auth/" >> $JBOSS_HOME/bin/standalone.conf
+echo "set KEYCLOAK_AUTH_SERVER_URL=https://keycloak-gepardec.apps.p.aws.ocp.gepardec.com/auth/" >> $JBOSS_HOME/bin/standalone.conf.bat
