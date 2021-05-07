0b9191b25b2f61c3c587cf24a3baa8c3e501abe7
Erhard Siegl <erhard.siegl@gepardec.com>
2021-05-07 21:21:16 +0200

    Feedback from review

--- Hogajama/configuration/local_configuration/01_set_experimental_messaging.sh
+++
@@ -1,3 +0,0 @@
-echo 'JAVA_OPTS="$JAVA_OPTS -Djboss.as.reactive.messaging.experimental=true"' >> $JBOSS_HOME/bin/standalone.conf
-echo 'set "JAVA_OPTS=%JAVA_OPTS% -Djboss.as.reactive.messaging.experimental=true"' >> $JBOSS_HOME/bin/standalone.conf.bat
-
--- Hogajama/hogajama-rs/src/main/java/com/gepardec/hogarama/service/WateringKafkaEndpoint.java
+++ Hogajama/hogajama-rs/src/main/java/com/gepardec/hogarama/service/WateringKafkaEndpoint.java
@@ -2,15 +2,12 @@ package com.gepardec.hogarama.service;
 
 import java.io.IOException;
 
-import javax.ejb.ActivationConfigProperty;
-import javax.ejb.MessageDriven;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.jms.BytesMessage;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageListener;
-// import org.jboss.ejb3.annotation.ResourceAdapter;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gepardec.hogarama.domain.metrics.Metrics;
@@ -19,10 +16,6 @@ import com.gepardec.hogarama.domain.sensor.SensorNormalizer;
 import com.gepardec.hogarama.domain.sensor.SensorProperties;
 import com.gepardec.hogarama.domain.unitmanagement.cache.SensorCache;
 import com.gepardec.hogarama.domain.watering.WateringService;
-
-import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class WateringKafkaEndpoint {
@@ -37,23 +30,23 @@ public class WateringKafkaEndpoint {
 
     @Inject
     SensorNormalizer sensorNormalizer;
-    
+
     @Inject
     SensorCache sensors;
-    
+
     @Incoming("habarama-in")
-	public void onMessage(String message) {
-        if ( !config.useKafkaWatering() ) {
+    public void onMessage(String message) {
+        if (!config.useKafkaWatering()) {
             return;
         }
-	    log.info("Receive message from habarama-in: " + message);
-		try {
-		    
-		    ObjectMapper mapper = new ObjectMapper();
+        log.info("Receive message from habarama-in: " + message);
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();
             SensorData sensorData = sensorNormalizer.normalize(mapper.readValue(message, SensorData.class));
 
             SensorProperties sensorProps = new SensorProperties(sensorData, sensors);
- 
+
             Metrics.sensorValues.labels(
                     sensorProps.getSensorName(), 
                     sensorProps.getDeviceId(), 
@@ -61,9 +54,9 @@ public class WateringKafkaEndpoint {
                     ).set(sensorData.getValue());
 
             wateringSvc.water(sensorData);
-            
-		} catch ( IOException e) {
-			throw new RuntimeException("Error handling sensor data!", e);
-		}
-	}
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error handling sensor data!", e);
+        }
+    }
 }
