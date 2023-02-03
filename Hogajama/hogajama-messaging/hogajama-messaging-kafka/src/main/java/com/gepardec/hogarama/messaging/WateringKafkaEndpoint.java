4a8b4520b02f29db75e281411bf5f740ea633ee8
Erhard Siegl <erhard.siegl@gepardec.com>
2023-02-03 21:55:49 +0100

    Fix merge problem

--- Hogajama/hogajama-messaging/hogajama-messaging-kafka/src/main/java/com/gepardec/hogarama/messaging/WateringKafkaEndpoint.java
+++ Hogajama/hogajama-messaging/hogajama-messaging-kafka/src/main/java/com/gepardec/hogarama/messaging/WateringKafkaEndpoint.java
@@ -25,10 +25,6 @@ public class WateringKafkaEndpoint {
     @Transactional
     @Incoming("habarama-in")
     public void onMessage(String message) {
-        if (!config.useKafkaWatering()) {
-            log.debug("Ignore message because useKafkaWatering is turned of");
-            return;
-        }
         log.info("Receive message from habarama-in: " + message);
         try {
 
