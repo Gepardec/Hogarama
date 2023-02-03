a160e46f7fe160ece32260beecbab04d7b56a28f
Egor Erofeev <egor.erofeev@gepardec.com>
2023-02-03 14:43:26 +0100

    First implementation of REST SensorDataMessagingEndpoint

---
+++ Hogajama/hogajama-messaging/hogajama-messaging-rest/src/main/java/com/gepardec/hogarama/messaging/JaxRsActivator.java
@@ -0,0 +1,12 @@
+package com.gepardec.hogarama.messaging;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Activating JAX-RS annotation based.
+ */
+@ApplicationPath("/rest")
+public class JaxRsActivator extends Application {
+
+}
---
+++ Hogajama/hogajama-messaging/hogajama-messaging-rest/src/main/java/com/gepardec/hogarama/messaging/SensorDataMessagingEndpoint.java
@@ -0,0 +1,19 @@
+package com.gepardec.hogarama.messaging;
+
+import com.gepardec.hogarama.domain.sensor.SensorData;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/messaging/sensorData")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public interface SensorDataMessagingEndpoint {
+
+    @POST
+    Response putMessage(SensorData sensorData);
+}
