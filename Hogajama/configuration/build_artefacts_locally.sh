97b0ef54ef79cd1cfc0d9c23d5811d6961950e28
Erhard Siegl <erhard.siegl@gepardec.com>
2023-10-27 16:19:45 +0200

    Install with 29.0.1.Final-jdk11

--- Hogajama/configuration/build_artefacts_locally.sh
+++ Hogajama/configuration/build_artefacts_locally.sh
@@ -7,4 +7,4 @@ HOGAJAMA_HOME=$DIR/..
 cd $HOGAJAMA_HOME
  . $ENV_FILE
 echo "Build Artefact Container-Image hogarama-build-artefacts in $HOGAJAMA_HOME with $MAVEN_ARGS"
-s2i build -e "MAVEN_ARGS=$MAVEN_ARGS" . quay.io/wildfly/wildfly-s2i-jdk11 hogarama-build-artefacts --incremental --loglevel=1
+s2i build -e "MAVEN_ARGS=$MAVEN_ARGS" . quay.io/wildfly/wildfly-s2i-jdk17 hogarama-build-artefacts --incremental --loglevel=1
--- Hogajama/configuration/install-to-vshn.sh
+++ Hogajama/configuration/install-to-vshn.sh
@@ -3,10 +3,10 @@
 # Create a JBSS Instance hogavshn with environment vshn.env
 # You need a cleanly checked out hogajama-angular-frontend. Dont build it locally before running this!
 
-hogavshn configure build_artefacts_locally.sh
-hogavshn configure build_container_locally.sh
+hogavshn configure build_artefacts_locally.sh || exit -1
+hogavshn configure build_container_locally.sh || exit -1
 
-docker tag hogajama-run gepardec/hogajama-run:latest
-docker push gepardec/hogajama-run:latest
+docker tag hogajama-run gepardec/hogajama-run:latest || exit -1
+docker push gepardec/hogajama-run:latest || exit -1
 
-oc import-image hogajama-run --from=gepardec/hogajama-run --all --confirm
+oc import-image hogajama-run --from=gepardec/hogajama-run --all --confirm || exit -1
